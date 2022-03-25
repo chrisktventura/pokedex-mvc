1256147bb6bc89119bbac6e9c7747b01f9af2063
Christopher Ventura <chrisventura@outlook.com.br>
2022-03-24 22:41:50 -0300

    +1

--- index.js
+++ index.js
@@ -1,40 +1,116 @@
+require("dotenv").config();
 const express = require("express");
+const { url } = require("inspector");
 const path = require("path");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
-const pokedex = [
-{   
-    id: 1,
-    nome: "Bulbassaur",
-    descricao: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger",
-    tipo: "Grass",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
-},
-{   
-    id: 2,
-    nome: "Charmander",
-    descricao: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-    tipo: "Fire",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
-},
-{   
-    id: 3,
-    nome: "Squirtle",
-    descricao: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-    tipo: "Water",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
-}
+const pokedex = [{
+        id: 1,
+        nome: "Bulbasaur",
+        tipo: "Grass",
+        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        descricao: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+        altura: "0.7 m",
+        peso: "6.9 kg",
+        categoria: "Seed",
+        habilidade: "Overgrow"
+
+    },
+    {
+        id: 2,
+        nome: "Ivysaur",
+        tipo: "Grass",
+        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+        descricao: "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.",
+        altura: "1.0 m",
+        peso: "13 kg",
+        categoria: "Seed",
+        habilidade: "Overgrow"
+    },
+    {
+        id: 3,
+        nome: "Venusaur",
+        tipo: "Grass",
+        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+        descricao: "Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.",
+        altura: "2.0 m",
+        peso: "100.0 kg",
+        categoria: "Seed",
+        habilidade: "Overgrow"
+    },
 ];
 
+let pokemon = {
+    id: "",
+    nome: "",
+    tipo: "",
+    imagem: "",
+    descricao: "",
+    altura: "",
+    peso: "",
+    categoria: "",
+    habilidade: ""
 
+}
 
+// Rotas
+
+//index - home
 app.get("/", (req, res) => {
-  res.render("index", {pokedex});
+    res.render("index", { pokedex, pokemon });
+});
+//contato 
+app.get("/contato", (req, res) => {
+    res.render("contato")
+});
+
+//detalhes
+app.get("/detalhes/:id", (req, res) => {
+    const id = +req.params.id;
+    pokemon = pokedex.find((pokemon) => pokemon.id === id);
+    res.render("detalhes", { pokemon });
+});
+
+//cadastro
+app.post("/cadastro", (req, res) => {
+    const pokemon = req.body;
+    pokemon.id = pokedex.length + 1;
+    pokedex.push(pokemon);
+
+});
+
+app.get("/cadastro", (req, res) => {
+    res.render("cadastro", { pokemon });
+})
+//editar
+app.get("/cadastro/:id", (req, res) => {
+    pokemon = pokedex.find((pok) => pok.id === +req.params.id);    
+    res.render("cadastro", { pokemon });
+})
+
+app.post("/update/:id", (req, res) => {
+    const id = +req.params.id - 1;
+    const newPokemon = req.body;
+    newPokemon.id = id + 1;
+    pokedex[id] = newPokemon;
+    pokemon = undefined;
+    res.redirect("/#cards");
 });
 
 
+//deletar 
+app.get("/delete/:id", (req, res) => {
+    const id = +req.params.id - 1;
+    delete pokedex[id];
+    res.redirect("/#cards");
+});
 
-app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
+app.listen(port, () =>
+    console.log(`Servidor rodando em http://localhost:${port}`)
+);
